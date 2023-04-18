d8c95f2b28c200e22cd8414244f753dd1661e485
Vadym Kuntso <95417173+VadymKuntso1@users.noreply.github.com>
2023-04-18 18:35:39 +0300

    second123

--- main/main/settings.py
+++ main/main/settings.py
@@ -10,9 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-#first
+#first123
 
-# second
+# second123
 
 from pathlib import Path
 
